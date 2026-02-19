ddfcad4085775fd25819c07f2081d53b33e89bab
krisha2551 <krishajasani62@gmail.com>
2026-02-19 13:31:28 +0530

    Add files via upload

--- Ch-7 MongoDB-Student Management System/controller/StudentController.js
+++ Ch-7 MongoDB-Student Management System/controller/StudentController.js
@@ -1,11 +1,19 @@
 import Student from "../model/StudentModel.js";
+
 import HttpError from "../middleware/httpError.js";
 
 // CREATE STUDENT
 const createStudent = async (req, res, next) => {
   try {
-    const { firstName, lastName, email, phoneNumber, course, isActive } =
-      req.body;
+
+    const { 
+      firstName, 
+      lastName, 
+      email, 
+      phoneNumber, 
+      course, 
+      isActive 
+    } = req.body;
 
     const student = new Student({
       firstName,
@@ -31,8 +39,10 @@ const createStudent = async (req, res, next) => {
 };
 
 // GET ALL STUDENTS
+
 const allStudent = async (req, res, next) => {
   try {
+
     const students = await Student.find({});
 
     if (!students || students.length === 0) {
@@ -48,9 +58,11 @@ const allStudent = async (req, res, next) => {
   }
 };
 
-// GET STUDENT BY ID
+// GET STUDENT BY 
+
 const StudentById = async (req, res, next) => {
   try {
+
     const { id } = req.params;
 
     const student = await Student.findById(id);
@@ -71,8 +83,60 @@ const StudentById = async (req, res, next) => {
   }
 };
 
+
+// UPDATE STUDENT (PATCH)
+
+const patchStudent = async (req, res, next) => {
+  try {
+
+    const { id } = req.params.id;
+
+    const student = await Student.findByIdAndUpdate(
+      id,
+      req.body,
+      { 
+        new: true
+
+      }
+    );
+
+    
+  }
+};
+
+
+
+// DELETE STUDENT
+
+const deleteStudent = async (req, res, next) => {
+  try {
+
+    const { id } = req.params;
+
+    const student = await Student.findByIdAndDelete(id);
+
+    if (!student) {
+      return next(new HttpError("Student not found", 404));
+    }
+
+    res.status(200).json({
+      message: "Student deleted successfully",
+      student,
+    });
+  } catch (error) {
+    
+    if (error.name === "CastError") {
+      return next(new HttpError("Invalid student ID", 400));
+    }
+    next(new HttpError(error.message, 500));
+  }
+};
+
+
+
 export default {
   createStudent,
   allStudent,
   StudentById,
+  deleteStudent,
 };
--- Ch-7 MongoDB-Student Management System/routes/StudentRoute.js
+++ Ch-7 MongoDB-Student Management System/routes/StudentRoute.js
@@ -1,15 +1,21 @@
 import express from "express";
-
 import StudentController from "../controller/StudentController.js";
 
 const router = express.Router();
 
+// CREATE STUDENT
 router.post("/", StudentController.createStudent);
 
+// GET ALL STUDENTS
 router.get("/allStudentData", StudentController.allStudent);
 
+// GET STUDENT BY ID
+router.get("/:id", StudentController.StudentById);
+
+
+
+// DELETE STUDENT
+router.delete("/:id", StudentController.deleteStudent);
 
 
 export default router;
-
-
