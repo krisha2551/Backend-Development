bc2d326c5075ddd23396713f93e2e9d283242148
krisha2551 <krishajasani62@gmail.com>
2026-03-26 13:48:52 +0530

    Add files via upload

--- 12-QuickNest/routes/userRoutes.js
+++ 12-QuickNest/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userController from "../controllers/userController.js";
+import validate from "../middleware/validate.js";
+import registerSchema from "../validation/registerSchema.js";
 
 const router = express.Router();
 
 // ADD USER
-router.post("/add", userController.add);
+router.post("/add", validate(registerSchema), userController.add);
 
 // LOGIN USER
 router.post("/login", userController.login);
