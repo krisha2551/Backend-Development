9e94190db38a5fbb0c34cda1be8b7a337e80af88
krisha2551 <krishajasani62@gmail.com>
2026-04-15 11:59:24 +0530

    Add files via upload

---
+++ 12-QuickNest/bookingRoutes.js
@@ -0,0 +1,56 @@
+import express from "express";
+import bookingController from "../controllers/bookingController.js";
+import auth from "../middleware/auth.js";
+
+
+const router = express.Router();
+
+
+// CREATE BOOKING
+router.post(
+  "/create",
+  auth,
+  bookingController.createBooking
+);
+
+
+// GET ALL BOOKINGS
+router.get(
+  "/all",
+  auth,
+  bookingController.getAllBooking
+);
+
+
+// GET own bookings (customer)
+router.get(
+  "/user",
+   auth, 
+   bookingController.getBookingsByUserId);
+
+
+// GET BOOKINGS BY SERVICE ID
+router.get(
+  "/service/:id",
+  auth,
+  bookingController.getAllService
+);
+
+
+// GET BOOKING BY ID
+router.get(
+  "/:id",
+  auth,
+  bookingController.getBookingById
+);
+
+
+// GET bookings by userId (admin)
+router.get(
+  "/user/:id", 
+  auth, 
+  bookingController.getBookingsByUserId
+);
+
+
+export default router;
